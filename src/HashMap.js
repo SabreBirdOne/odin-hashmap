2aad653c6463e984953b3364654eb7b4aa587e3f
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-07-06 16:40:03 -0400

    Add HashMap starter code and specs

--- src/HashMap.js
+++ src/HashMap.js
@@ -3,7 +3,53 @@ export default class HashMap {
 
     }
 
-    set(key, value){
-        
+    hash(key){
+        /* takes a key to produce a hash code to a bucket*/
     }
+
+    set(key, value){
+        /*
+        takes two arguments: the first is a key, and the second is a value that is assigned to this key. If a key already exists, then the old value is overwritten
+        when a collision occurs, the key-value pair is prepended to the bucket it is hashed to
+        when loadFactor is reached (this.length + 1 === loadFactor), double the number of buckets and rehash elements
+        */
+    }
+
+    get(key){
+        /* takes a key and returns the value assigned to this key. 
+        If the key is not found, return null.*/
+    }
+
+    has(key){
+        /* returns true if the key is in the hash map, else false */
+    }
+
+    remove(key){
+        /* If the key is in the hash map, remove the entry with that key and return true. 
+        If the key isn’t in the hash map, returns false 
+        */
+    }
+
+    length() {
+        // returns the number of stored keys in the hash map.
+    } 
+
+    clear() {
+        // removes all entries in the hash map.
+    } 
+
+    keys() {
+        // returns an array containing all the keys inside the hash map.
+    }
+
+    values() {
+        // returns an array containing all the values.
+    }
+
+    entries() {
+        /* returns an array that contains each key, value pair. 
+        Example: [[firstKey, firstValue], [secondKey, secondValue]]
+        A hash map does not preserve insertion order.
+        */
+    } 
 }
